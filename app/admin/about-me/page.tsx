c86498dd91f96babe7756b278084545a41a84851
gonzaloyacante <gyacante9@gmail.com>
2025-01-19 23:06:46 +0000

    Continuando con la pantala de admin y mejoras en bbdd

--- app/admin/about-me/page.tsx
+++ app/admin/about-me/page.tsx
@@ -1,10 +1,8 @@
-import { SobreMiAdmin } from "@/components/admin/SobreMiAdmin";
-
 export default function AdminSobreMi() {
   return (
     <div className="space-y-6">
       <h1 className="text-3xl font-bold">Editar Sobre Mí</h1>
-      <SobreMiAdmin />
+      <p>Próximamente</p>
     </div>
   );
 }
